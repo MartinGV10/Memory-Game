fd781d32ce92780fff0c39eface445b7ebf389e7
MartinGV10 <mganen10@gmail.com>
2025-12-15 13:28:22 -0500

    Added base page without items

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,19 @@ function App() {
 
   return (
     <>
+      <div className="top">
+        <h1 className="title">Memory Game</h1>
+        <h3 className="sub">Get points by clicking on an image but don't click on the same one more than once!</h3>
+      </div>
 
+      <div className="cont">
+
+      </div>
+
+      <div className="pts">
+        <p className="score">Score: </p>
+        <p className="score">High Score: </p>
+      </div>
     </>
   )
 }
